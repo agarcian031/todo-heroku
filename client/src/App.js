3c9ac943696512f0332d1e457c60841f1f81c319
Amanda Garcia <algarcian@outlook.com>
2019-06-20 11:51:28 -0600

    Add axios calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'; 
 import {Form, Header, Container, List, Segment} from 'semantic-ui-react'; 
 import './App.css';
 
@@ -6,11 +7,32 @@ function App() {
   const [name, setName] = useState("");
   const [todos, setTodos] = useState(["Buy Milk", "Buy Eggs", "Buy Kittens"])
 
+  useEffect( () => {
+    axios.get("/api/todos")
+    .then(res => {
+      setTodos(res.data)
+    })
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault(); 
-    setTodos([...todos, name, ])
-    setName("")
+    axios.post("/api/todos", {name})
+    .then( res => {
+      setTodos([...todos, res.data, ])
+      setName("")
+    })
+  }
+
+  const updateTodo = (id) => {
+    axios.put( `/api/todos/${id}`)
+    .then( res => {
+      const newTodos = todos.map(t => {
+        if(t.id === id)
+          return res.data 
+        return t; 
+      })
+      setTodos(newTodos); 
+    })
   }
 
   return (
@@ -26,12 +48,19 @@ function App() {
             />
           </Form>
           <List>
-            {todos.map((t, i) => <List.Item key={i}>{t}</List.Item>)}
+            {todos.map((t, i) => <List.Item key={t.id} style={t.complete && styles.complete} onClick={() => updateTodo(t.id)}>{t.name}</List.Item>)}
           </List>
       </Segment>
       
     </Container>
   );
+}
+
+const styles = {
+  complete: {
+    textDecoration: "line-through", 
+    color: "grey",
+  }
 }
 
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :todos, except: :show 
+  end
+
+  get '*other', to: 'static#index'
+
+  #https://mywebsite.com
 end
